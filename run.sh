54bd8faa0c3557da8ad5061ad374f574a70203ee
Jamie Wilkinson <jamie@jamiedubs.com>
2018-10-30 08:18:42 -0400

    Cache both XML and JSON responses

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 ./download-gml.sh
+./download-gson.sh
 
-dir="data-json"
-ipfs add --nocopy --quiet --recursive "$dir" 2>&1 | tee ipfs-hashes
+ipfs add --nocopy --quiet --recursive "data" 2>&1 | tee ipfs-hashes
+cat ipfs-hashes | shuf | ./ipfs-gml-cache.sh
 
+ipfs add --nocopy --quiet --recursive "data-json" 2>&1 | tee ipfs-hashes
 cat ipfs-hashes | shuf | ./ipfs-gml-cache.sh
