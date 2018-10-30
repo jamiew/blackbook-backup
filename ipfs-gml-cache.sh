894ff8e0530dd61301d80fdbaf9e5ca5b2f14e27
Jamie Wilkinson <jamie@jamiedubs.com>
2018-10-30 08:18:28 -0400

    Warm using Cloudflare's IPFS gateway too

--- ipfs-gml-cache.sh
+++ ipfs-gml-cache.sh
@@ -5,6 +5,7 @@ while read hash; do
 
   wget -t 2 -T 3 https://ipfs.io/ipfs/$hash -O /dev/null
   wget -t 2 -T 3 https://ipfs.infura.io/ipfs/$hash -O /dev/null
+  wget -t 2 -T 3 https://cloudflare-ipfs.com/ipfs/$hash -O /dev/null
 
   # wget -T 2 https://xmine128.tk/ipfs/$hash -O /dev/null
   # wget -T 2 https://ipfs.jes.xxx/ipfs/$hash -O /dev/null 
