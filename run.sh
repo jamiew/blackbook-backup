33ad874e83bcbcb2a7edbc66f61eeb626b408c7c
Jamie Wilkinson <jamie@jamiedubs.com>
2021-02-18 14:59:15 +0000

    Use --fscache and time each command

--- run.sh
+++ run.sh
@@ -3,10 +3,15 @@
 ./download-gml.sh
 ./download-gson.sh
 
-ipfs add --nocopy --quiet --recursive "data" 2>&1 | tee ipfs-hashes-gml
-ipfs add --nocopy --quiet --recursive "data-json" 2>&1 | tee ipfs-hashes-json
+echo "Adding .gml files..."
+time ipfs add --nocopy --fscache --quiet --recursive "data" 2>&1 | tee ipfs-hashes-gml
+
+echo "Adding .json files..."
+time ipfs add --nocopy --fscache --quiet --recursive "data-json" 2>&1 | tee ipfs-hashes-json
 
 # cat ipfs-hashes-gml | shuf | ./ipfs-gml-cache.sh
 # cat ipfs-hashes-json | shuf | ./ipfs-gml-cache.sh
 
 # TODO capture the output of either data/ or data-json/ and publish to IPNS
+
+echo "All done"
